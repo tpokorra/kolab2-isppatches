5d830a02256acb6730fec86e1ec6929c3df8b538
TBits.net GmbH <mailinglists@tbits.net>
2010-05-18 14:18:56 +0200

    checking password against regex

--- kolab-webadmin/kolab-webadmin/php/admin/include/passwd.php
+++ kolab-webadmin/kolab-webadmin/php/admin/include/passwd.php
@@ -25,12 +25,33 @@ function gensalt()
   return $salt;
 }
 
+// get regex for password validation
+function pwSecRegEx($dn) {
+	global $ldap;
+	global $dn; // i'm pretty sure that's nonsense...  -- simon
+	if(preg_match("/cn=internal," . $_SESSION['base_dn'] . "/", $dn))
+		$attrs = array('passwordSecAdminRegEx');
+	else
+		$attrs = array('passwordSecUserRegEx');
+	if($ldap->search("k=kolab,".$_SESSION['base_dn'],
+			'(objectclass=kolab)', $attrs)) {
+		$entry = $ldap->getEntries();
+		return $entry[0][strtolower(end($attrs))][0];
+	}
+	else
+		return false;
+}
+
 // Check that passwords from form input match
 function checkpw( $form, $key, $value ) {
   global $action;
+  $passwordRegEx = pwSecRegEx();
   if( $action == 'firstsave' ) {
     if( $key == 'password_0' ) {
       if( $value == '' ) return _('Password is empty');
+	  elseif(!preg_match($passwordRegEx, $value)
+			&& $key == 'password_0' && !empty($passwordRegEx))
+		return _('Password does not meet password policy requirements');
     } else if( $key == 'password_1' ) {
       if( $value != $_POST['password_0'] ) {
         return _('Passwords dont match');
@@ -40,6 +61,10 @@ function checkpw( $form, $key, $value ) {
     if( $value != $_POST['password_0'] ) {
       return _('Passwords dont match');
     }
+	elseif($passwordRegEx && $key == 'password_0' && !empty($value)) {
+		if(!preg_match("$passwordRegEx", $value))
+			return _('Password does not meet security policy requirements');
+	}
   }
   return '';
 }
