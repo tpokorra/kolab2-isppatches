d74000e696c2cbc71e146274b438063a296c09c1
TBits.net GmbH <mailinglists@tbits.net>
2010-05-21 17:05:17 +0200

    added missing ';' in Conf.pm

--- perl-kolab/perl-kolab/lib/Kolab/Conf.pm
+++ perl-kolab/perl-kolab/lib/Kolab/Conf.pm
@@ -630,7 +630,7 @@ sub rebuildTemplates
 
     my $templatedir = $Kolab::config{"templatedir"};
 
-    mergeCustomHostSettings()
+    mergeCustomHostSettings();
 
     Kolab::log('T', 'Regenerating configuration files', KOLAB_DEBUG );
 
